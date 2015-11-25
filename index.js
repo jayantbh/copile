1c48c729896e87a43fe01d25e18ce9ad46f740bb
Jayant Bhawal <bhawal.jayant@gmail.com>
2015-11-26 01:23:53 +0530

    First usable release. Works local for now.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var fs = require('fs');
+var exec = require('child_process').exec;
+var Q = require('q');
 
 app.use(express.static("public/"));
 app.use(express.static(__dirname));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -15,35 +18,67 @@ app.use(function(req, res, next) {
 var roomId = 0;
 app.get('/:room', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
-    console.log('Page loaded with ID '+roomId);
-    if(req.params.room)
-        roomId = req.params.room;
+    console.log('Page loaded with ID ' + roomId);
+    if (req.params.room) {
+        if (req.params.room != "favicon.ico") {
+            roomId = req.params.room;
+        }
+    }
 });
 io.on('connection', function (socket) {
     console.log('a user connected');
 
     socket.join(roomId);
-    var username;
-    socket.on('new user',function(name){
-        username = name;
-        console.log(name+" has connected");
-        io.to(roomId).emit('system log',username+" has connected.")
-    })
-    socket.on('disconnect', function () {
-        console.log(username+' disconnected');
-        io.to(roomId).emit('system log',username+" disconnected.")
+    socket.on('code submission', function (code) {
+
+        //console.log(code);
+        var filename = new Date().getTime();
+        var filename_ext = filename + ".c";
+        var filename_out = filename;
+        var path = "./compiled/" + filename_ext;
+        var command = "gcc " + path + " -o ./compiled/" + filename_out;
+        var path_out = "./compiled/" + filename_out;
+        var delete_command = "rm "+path;
+        var delete_command_out = "rm "+path_out;
+        fs.writeFile(path, code, function (err) {
+            //var deferred = Q.defer();
+            if (err) {
+                return console.log(err);
+            }
+            exec(command, function (error, stdout, stderr) {
+                if(error){
+                    console.error(error);
+                    io.to(roomId).emit("shell output","Compilation failed.\n");
+                }
+                if(stderr){
+                    console.error(stderr);
+                    io.to(roomId).emit("shell output",stderr);
+                }
+                if(stdout){
+                    console.log(stdout);
+                    io.to(roomId).emit("shell output",stdout);
+                }
+                else{
+                    exec(path_out, function (error, stdout, stderr) {
+                        if(error){
+                            console.error(error);
+                            //io.to(roomId).emit("shell output","Execution failed.");
+                        }
+                        else if(stderr){
+                            console.error(stderr);
+                            io.to(roomId).emit("shell output",stderr);
+                        }
+                        else{
+                            //success
+                            console.log(stdout);
+                            io.to(roomId).emit("shell output",stdout);
+                        }
+                    });
+                }
+            });
+        });
     });
-    socket.on('chat message',function(msg){
-        console.log("Message: "+msg);
-        io.to(roomId).emit('chat message','<b>'+username+'</b>: '+msg);
-    });
-    socket.on('typing',function(name){
-        io.to(roomId).emit('system event',name);
-    })
-    socket.on('not typing',function(name){
-        io.to(roomId).emit('not typing',name);
-    })
 });
 http.listen(port, function () {
-    console.log('listening on port:'+port);
+    console.log('listening on port:' + port);
 });
---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+/**
+ * Created by jayantbhawal on 25/11/15.
+ */
+function ready(fn) {
+    if (document.readyState != 'loading'){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+(function () {
+    editor.setValue("#include \<stdio.h\>\nint main\(\)\{\n\t\n\treturn 0;\n}");
+    ready(function () {
+        editor.focus(); //To focus the ace editor
+        var n = editor.getSession().getValue().split("\n").length; // To count total no. of lines
+        editor.gotoLine(n-2); //Go to end of document
+        editor.navigateLineEnd(); // Navigate to end of line
+    });
+    var socket = io();
+    document.querySelector("#submit").onclick = function () {
+        var code = editor.getValue();
+        //console.log(code);
+
+        socket.emit('code submission', code);
+    };
+
+    socket.on("shell output", function (reply) {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+
+        now = h + ':' + m + ':' + s;
+
+        console.log(reply);
+        document.querySelector("#output").innerHTML = now + " >> " + reply + "<br>" + document.querySelector("#output").innerHTML;
+    });
+})();
